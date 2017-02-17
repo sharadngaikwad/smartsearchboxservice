e33f01cf88f1d230dc186b1f3ac0e0faf5284c37
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-02-17 14:22:45 +0530

    Added comments

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@
  * Created by s_rya on 2/13/2017.
  */
 'use strict';
+
+//Config values for Watson Conversation Service running in Bluemix
 const watsonCoversation = {
     url: 'https://gateway.watsonplatform.net/conversation/api',
     version_date: '2016-10-21',
@@ -11,15 +13,17 @@ const watsonCoversation = {
     workspacePassword: 'dQ1QR7W26I3Y'
 };
 
+//Config values for Watson Discovery Service running in Bluemix
 const watsonDiscovery = {
     username: 'cce8943a-25a5-4cdf-bb87-053ed95b3214',
     password: 'y0oMfqx6vRo3',
     version: 'v1',
     version_date: '2016-12-01',
     environment_id: '36eee3d4-fa56-4712-85d7-a0e1b34e93c3',
+    defaultCollection: 'customerDB-UK',
     collection_id: {
-        'customerDB-UK' : '9a53f68c-22ab-4cff-9708-d0887247dedf',
-        'cars' : '52e46aeb-3d4a-4d0d-8a5f-69485a3285da'
+        'customerDB-UK': '9a53f68c-22ab-4cff-9708-d0887247dedf',
+        'cars': '52e46aeb-3d4a-4d0d-8a5f-69485a3285da'
     }
 };
 
